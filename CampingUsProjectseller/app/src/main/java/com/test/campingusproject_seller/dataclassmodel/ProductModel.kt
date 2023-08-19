bb94b7c29863adf582c0062a2a78b213cfe68745
nueijeel <lge6716@gmail.com>
2023-08-19 22:02:18 +0900

    feat : 상품 모델 생성
    ProductModel.kt

---
+++ CampingUsProjectseller/app/src/main/java/com/test/campingusproject_seller/dataclassmodel/ProductModel.kt
@@ -0,0 +1,13 @@
+package com.test.campingusproject_seller.dataclassmodel
+
+data class ProductModel (
+    var productId : Long,                   //상품 ID
+    var productSellerId : String,           //상품 판매자 ID
+    var productName : String,               //상품 이름
+    var productPrice : Long,                //상품 가격
+    var productImage : String,              //상품 이미지
+    var productInfo : String,               //상품 정보
+    var productCount : Long,                //상품 수량
+    var productSellingStatus : Boolean,     //상품 판매상태
+    var productDiscountRate : Long,         //상품 할인율
+    var productRecommendationCount : Long)  //상품 추천 수
