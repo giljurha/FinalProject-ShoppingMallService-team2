8faa15e29b514eca7b3bab51fadf300277e237d4
Do-Re-Min-Woo <kimy5878@naver.com>
2023-08-23 18:06:07 +0900

    merge

--- CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/main/MainActivity.kt
+++ CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/main/MainActivity.kt
@@ -14,6 +14,7 @@ import com.test.campingusproject_customer.ui.comunity.PostWriteFragment
 import com.test.campingusproject_customer.ui.comunity.ComunityFragment
 import com.test.campingusproject_customer.ui.myprofile.MyprofileFragment
 import com.test.campingusproject_customer.ui.shopping.ShoppingFragment
+import com.test.campingusproject_customer.ui.shopping.ShoppingPlusFragment
 
 class MainActivity : AppCompatActivity() {
     lateinit var activityMainBinding: ActivityMainBinding
@@ -27,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         val CAMPSITE_FRAGMENT = "CampsiteFragment"
         val SHOPPING_PRODUCT_FRAGMENT = "ShoppingProductFragment"
         val POST_WRITE_FRAGMENT = "PostWriteFragment"
+        val SHOPPING_PLUS_FRAGMENT = "ShoppingPlusFragment"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -89,6 +91,7 @@ class MainActivity : AppCompatActivity() {
             MYPROFILE_FRAGMENT -> MyprofileFragment()
             CAMPSITE_FRAGMENT->CampsiteFragment()
             POST_WRITE_FRAGMENT -> PostWriteFragment()
+            SHOPPING_PLUS_FRAGMENT -> ShoppingPlusFragment()
             else -> Fragment()
         }
 
--- CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/shopping/ShoppingPlusFragment.kt
+++ CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/shopping/ShoppingPlusFragment.kt
@@ -1,6 +1,5 @@
 package com.test.campingusproject_customer.ui.shopping
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,31 +7,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.OnBackPressedCallback
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.test.campingusproject_customer.R
-import com.test.campingusproject_customer.databinding.FragmentShoppingBinding
+import com.test.campingusproject_customer.databinding.FragmentShoppingPlusBinding
 import com.test.campingusproject_customer.databinding.HeaderShoppingBinding
 import com.test.campingusproject_customer.databinding.RowShoppingBinding
+import com.test.campingusproject_customer.databinding.RowShoppingPlusBinding
 import com.test.campingusproject_customer.ui.main.MainActivity
 
-class ShoppingFragment : Fragment() {
-    lateinit var fragmentShoppingBinding: FragmentShoppingBinding
+class ShoppingPlusFragment : Fragment() {
+    lateinit var fragmentShoppingPlusBinding: FragmentShoppingPlusBinding
     lateinit var mainActivity: MainActivity
-    lateinit var callback: OnBackPressedCallback
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        fragmentShoppingPlusBinding = FragmentShoppingPlusBinding.inflate(layoutInflater)
         mainActivity = activity as MainActivity
-        fragmentShoppingBinding = FragmentShoppingBinding.inflate(layoutInflater)
 
-        fragmentShoppingBinding.run {
+        fragmentShoppingPlusBinding.run {
             // 툴바
-            toolbarShopping.run {
-                title = "쇼핑"
+            toolbarShoppingPlus.run {
+                title = "흐음.."
 
                 // drawerLayout 설정
                 setNavigationIcon(R.drawable.menu_24px)
@@ -40,7 +38,7 @@ class ShoppingFragment : Fragment() {
                     drawerLayoutShopping.open()
                 }
             }
-            navigationViewShopping.run {
+            navigationViewShoppingPlus.run {
                 //헤더 설정
                 val headerShoppingBinding = HeaderShoppingBinding.inflate(inflater)
                 headerShoppingBinding.textViewShoppingHeaderUserName.text = "김민우 님"
@@ -52,61 +50,61 @@ class ShoppingFragment : Fragment() {
                         // 특별
                         R.id.itemShoppingRealTimeRanking -> { // 실시간 랭킹
                             mainActivity.replaceFragment(MainActivity.SHOPPING_PLUS_FRAGMENT, false, true, null)
-                            toolbarShopping.title = "실시간 랭킹"
+                            toolbarShoppingPlus.title = "실시간 랭킹"
                             drawerLayoutShopping.close()
                         }
                         R.id.itemShoppingPopularitySale -> { // 인기특가
                             mainActivity.replaceFragment(MainActivity.SHOPPING_PLUS_FRAGMENT, false, true, null)
-                            toolbarShopping.title = "인기특가"
+                            toolbarShoppingPlus.title = "인기특가"
                             drawerLayoutShopping.close()
                         }
 
                         // 상품
                         R.id.itemShoppingTotalProduct -> { // 전체 상품
                             mainActivity.replaceFragment(MainActivity.SHOPPING_PLUS_FRAGMENT, false, true, null)
-                            toolbarShopping.title = "전체 상품"
+                            toolbarShoppingPlus.title = "전체 상품"
                             drawerLayoutShopping.close()
                         }
                         R.id.itemShoppingCap -> { // 모자
                             mainActivity.replaceFragment(MainActivity.SHOPPING_PLUS_FRAGMENT, false, true, null)
-                            toolbarShopping.title = "모자"
+                            toolbarShoppingPlus.title = "모자"
                             drawerLayoutShopping.close()
                         }
                         R.id.itemShoppingTop -> { // 상의
                             mainActivity.replaceFragment(MainActivity.SHOPPING_PLUS_FRAGMENT, false, true, null)
-                            toolbarShopping.title = "상의"
+                            toolbarShoppingPlus.title = "상의"
                             drawerLayoutShopping.close()
                         }
                         R.id.itemShoppingBottoms -> { // 하의
                             mainActivity.replaceFragment(MainActivity.SHOPPING_PLUS_FRAGMENT, false, true, null)
-                            toolbarShopping.title = "하의"
+                            toolbarShoppingPlus.title = "하의"
                             drawerLayoutShopping.close()
                         }
                         R.id.itemShoppingShoes -> { // 신발
                             mainActivity.replaceFragment(MainActivity.SHOPPING_PLUS_FRAGMENT, false, true, null)
-                            toolbarShopping.title = "신발"
+                            toolbarShoppingPlus.title = "신발"
                             drawerLayoutShopping.close()
                         }
 
                         // 캠핑용품
                         R.id.itemShoppingTent -> { // 텐트
                             mainActivity.replaceFragment(MainActivity.SHOPPING_PLUS_FRAGMENT, false, true, null)
-                            toolbarShopping.title = "텐트"
+                            toolbarShoppingPlus.title = "텐트"
                             drawerLayoutShopping.close()
                         }
                         R.id.itemShoppingStove -> { // 난로
                             mainActivity.replaceFragment(MainActivity.SHOPPING_PLUS_FRAGMENT, false, true, null)
-                            toolbarShopping.title = "난로"
+                            toolbarShoppingPlus.title = "난로"
                             drawerLayoutShopping.close()
                         }
                         R.id.itemShoppingFireWood -> { // 장작
                             mainActivity.replaceFragment(MainActivity.SHOPPING_PLUS_FRAGMENT, false, true, null)
-                            toolbarShopping.title = "장작"
+                            toolbarShoppingPlus.title = "장작"
                             drawerLayoutShopping.close()
                         }
                         R.id.itemShoppingContainer -> { // 용기
                             mainActivity.replaceFragment(MainActivity.SHOPPING_PLUS_FRAGMENT, false, true, null)
-                            toolbarShopping.title = "용기"
+                            toolbarShoppingPlus.title = "용기"
                             drawerLayoutShopping.close()
                         }
                     }
@@ -114,69 +112,52 @@ class ShoppingFragment : Fragment() {
                 }
             }
 
-            recyclerViewShoppingProduct.run {
-                adapter = ShoppingProductAdapter()
+            recyclerViewShoppingPlusProduct.run {
+                adapter = ShoppingPlusProductAdapter()
                 layoutManager = GridLayoutManager(context, 3)
             }
         }
-
-        return fragmentShoppingBinding.root
+        return fragmentShoppingPlusBinding.root
     }
 
     // 상품을 보여주는 리사이클러뷰
-    inner class ShoppingProductAdapter : RecyclerView.Adapter<ShoppingProductAdapter.ShoppingProductViewHolder>(){
-        inner class ShoppingProductViewHolder(rowShoppingBinding: RowShoppingBinding) : RecyclerView.ViewHolder(rowShoppingBinding.root){
-            val imageViewShoppingImage: ImageView
-            val textViewShoppingName: TextView
-            val textViewShoppingSize: TextView
-            val textViewShoppingPrice: TextView
-            val imageButtonLiked: ImageView
+    inner class ShoppingPlusProductAdapter : RecyclerView.Adapter<ShoppingPlusProductAdapter.ShoppingPlusProductViewHolder>(){
+        inner class ShoppingPlusProductViewHolder(rowShoppingPlusBinding: RowShoppingPlusBinding) : RecyclerView.ViewHolder(rowShoppingPlusBinding.root){
+            val imageViewShoppingPlusImage: ImageView
+            val textViewShoppingPlusName: TextView
+            val textViewShoppingPlusSize: TextView
+            val textViewShoppingPlusPrice: TextView
+            val imageButtonShoppingPlusLiked: ImageView
 
             init{
-                imageViewShoppingImage = rowShoppingBinding.imageViewShoppingImage
-                textViewShoppingName = rowShoppingBinding.textViewShoppingName
-                textViewShoppingSize = rowShoppingBinding.textViewShoppingSize
-                textViewShoppingPrice = rowShoppingBinding.textViewShoppingPrice
-                imageButtonLiked = rowShoppingBinding.imageButtonLiked
+                imageViewShoppingPlusImage = rowShoppingPlusBinding.imageViewShoppingPlusImage
+                textViewShoppingPlusName = rowShoppingPlusBinding.textViewShoppingPlusName
+                textViewShoppingPlusSize = rowShoppingPlusBinding.textViewShoppingPlusSize
+                textViewShoppingPlusPrice = rowShoppingPlusBinding.textViewShoppingPlusPrice
+                imageButtonShoppingPlusLiked = rowShoppingPlusBinding.imageButtonShoppingPlusLiked
             }
         }
 
-        override fun onCreateViewHolder( parent: ViewGroup,viewType: Int): ShoppingProductViewHolder {
-            val rowShoppingBinding = RowShoppingBinding.inflate(layoutInflater)
-            val shoppingProductViewHolder = ShoppingProductViewHolder(rowShoppingBinding)
+        override fun onCreateViewHolder( parent: ViewGroup,viewType: Int): ShoppingPlusFragment.ShoppingPlusProductAdapter.ShoppingPlusProductViewHolder {
+            val rowShoppingPlusBinding = RowShoppingPlusBinding.inflate(layoutInflater)
+            val shoppingPlusProductViewHolder = ShoppingPlusProductViewHolder(rowShoppingPlusBinding)
 
-            rowShoppingBinding.root.layoutParams = ViewGroup.LayoutParams(
+            rowShoppingPlusBinding.root.layoutParams = ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT
             )
-            return shoppingProductViewHolder
+            return shoppingPlusProductViewHolder
         }
 
         override fun getItemCount(): Int {
             return 9
         }
 
-        override fun onBindViewHolder(holder: ShoppingProductViewHolder, position: Int) {
-            holder.imageViewShoppingImage.setImageResource(R.drawable.ic_launcher_foreground)
-            holder.textViewShoppingName.text = "운동화"
-            holder.textViewShoppingSize.text = "250"
-            holder.textViewShoppingPrice.text = "30000원"
+        override fun onBindViewHolder(holder: ShoppingPlusProductAdapter.ShoppingPlusProductViewHolder, position: Int) {
+            holder.imageViewShoppingPlusImage.setImageResource(R.drawable.ic_launcher_foreground)
+            holder.textViewShoppingPlusName.text = "운동화"
+            holder.textViewShoppingPlusSize.text = "250"
+            holder.textViewShoppingPlusPrice.text = "30000원"
         }
-    }
-
-    //뒤로가기 버튼 눌렀을 때 동작할 코드 onDetech까지
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        callback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                mainActivity.removeFragment(MainActivity.SHOPPING_FRAGMENT)
-                mainActivity.activityMainBinding.bottomNavigationViewMain.selectedItemId = R.id.menuItemHome
-            }
-        }
-        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
-    }
-    override fun onDetach() {
-        super.onDetach()
-        callback.remove()
     }
 }
