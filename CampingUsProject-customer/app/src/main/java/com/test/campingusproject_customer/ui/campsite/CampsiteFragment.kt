dc685d2a6ef2945704a30b2b136c203f2e299177
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2023-08-23 12:01:04 +0900

    feat: 용진님 충돌 해결 후 캠핑장 코드 이식작업(#deveolop)
    어쩌다보니 develop에서 작업해버림....

--- CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/campsite/CampsiteFragment.kt
+++ CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/campsite/CampsiteFragment.kt
@@ -1,11 +1,13 @@
 package com.test.campingusproject_customer.ui.campsite
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
@@ -15,6 +17,7 @@ import com.test.campingusproject_customer.ui.main.MainActivity
 class CampsiteFragment : Fragment(), OnMapReadyCallback {
     lateinit var fragmentCampsiteBinding: FragmentCampsiteBinding
     lateinit var mainActivity:MainActivity
+    lateinit var callback: OnBackPressedCallback
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -38,5 +41,21 @@ class CampsiteFragment : Fragment(), OnMapReadyCallback {
     //지도객체가 callback되면 호출되는 메서드
     override fun onMapReady(p0: NaverMap) {
         Log.d("testt","지도 띄워짐")
+    }
+
+    //뒤로가기 버튼 눌렀을 때 동작할 코드 onDetech까지
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                mainActivity.removeFragment(MainActivity.CAMPSITE_FRAGMENT)
+                mainActivity.activityMainBinding.bottomNavigationViewMain.selectedItemId = R.id.menuItemHome
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+    override fun onDetach() {
+        super.onDetach()
+        callback.remove()
     }
 }
