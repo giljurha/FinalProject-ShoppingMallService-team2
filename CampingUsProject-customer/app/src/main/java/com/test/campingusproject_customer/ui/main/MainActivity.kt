598b85683f70cb9739df08ff9e830e6b4efab4a6
Do-Re-Min-Woo <kimy5878@naver.com>
2023-08-23 13:43:29 +0900

    fix : 오류해결을 위한 커밋

--- CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/main/MainActivity.kt
+++ CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/main/MainActivity.kt
@@ -8,11 +8,11 @@ import androidx.fragment.app.FragmentTransaction
 import com.test.campingusproject_customer.R
 import com.test.campingusproject_customer.databinding.ActivityMainBinding
 import com.test.campingusproject_customer.ui.campsite.CampsiteFragment
-//import com.test.campingusproject_customer.ui.shopping.ShoppingMainFragment
-//import com.test.campingusproject_customer.ui.shopping.ShoppingProductFragment
+import com.test.campingusproject_customer.ui.shopping.ShoppingProductFragment
 import com.test.campingusproject_customer.ui.camping.CampingFragment
 import com.test.campingusproject_customer.ui.comunity.ComunityFragment
 import com.test.campingusproject_customer.ui.myprofile.MyprofileFragment
+import com.test.campingusproject_customer.ui.shopping.ShoppingFragment
 
 class MainActivity : AppCompatActivity() {
     lateinit var activityMainBinding: ActivityMainBinding
@@ -24,7 +24,6 @@ class MainActivity : AppCompatActivity() {
         val COMUNITY_FRAGMENT = "ComunityFragment"
         val MYPROFILE_FRAGMENT = "MyProfileFragment"
         val CAMPSITE_FRAGMENT = "CampsiteFragment"
-        val SHOPPING_MAIN_FRAGMENT = "ShoppingMainFragment"
         val SHOPPING_PRODUCT_FRAGMENT = "ShoppingProductFragment"
     }
 
@@ -48,12 +47,11 @@ class MainActivity : AppCompatActivity() {
                         }
                         //캠핑장 클릭
                         R.id.menuItemCamping -> {
-//                            replaceFragment(CAMPING_FRAGMENT, false, false, null)
-                            replaceFragment(CAMPSITE_FRAGMENT,true,false,null)
+                            replaceFragment(CAMPSITE_FRAGMENT,false,false,null)
                         }
                         //쇼핑 클릭
                         R.id.menuItemShopping -> {
-                            replaceFragment(SHOPPING_MAIN_FRAGMENT, false, false, null)
+                            replaceFragment(SHOPPING_FRAGMENT, false, false, null)
                         }
                         //커뮤니티 클릭
                         R.id.menuItemComunity -> {
@@ -83,7 +81,8 @@ class MainActivity : AppCompatActivity() {
         var newFragment = when (name) {
             HOME_FRAGMENT -> HomeFragment()
             CAMPING_FRAGMENT -> CampingFragment()
-//            SHOPPING_MAIN_FRAGMENT -> ShoppingMainFragment()
+            SHOPPING_FRAGMENT -> ShoppingFragment()
+            SHOPPING_PRODUCT_FRAGMENT -> ShoppingProductFragment()
             COMUNITY_FRAGMENT -> ComunityFragment()
             MYPROFILE_FRAGMENT -> MyprofileFragment()
             CAMPSITE_FRAGMENT->CampsiteFragment()
