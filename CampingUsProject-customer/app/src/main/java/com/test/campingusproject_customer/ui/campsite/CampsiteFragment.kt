878956694f674100ba33b9108d8d3f9b68e22120
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2023-08-24 11:33:13 +0900

    fix: 제휴 캠핑장 백스택 및 바텀 네비게이션 식별 대응(2-16-55_contract_campsite_map)

--- CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/campsite/CampsiteFragment.kt
+++ CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/campsite/CampsiteFragment.kt
@@ -61,9 +61,9 @@ class CampsiteFragment : Fragment(), OnMapReadyCallback {
         //OnMapReadyCallback등록
         mapFragment.getMapAsync(this)
 
+        //제휴 캠핑장 클릭 시
         fragmentCampsiteBinding.buttonContractCampsite.setOnClickListener {
-            mainActivity.removeFragment(MainActivity.CAMPSITE_FRAGMENT)
-            mainActivity.replaceFragment(MainActivity.CONTRACT_CAMPSITE_FRAGMENT, false, false, null)
+            mainActivity.replaceFragment(MainActivity.CONTRACT_CAMPSITE_FRAGMENT, true, false, null)
         }
 
         return fragmentCampsiteBinding.root
