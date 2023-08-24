bbc41c2fa3ee87221c3a7a23f385429b778ba987
nueijeel <lge6716@gmail.com>
2023-08-24 11:50:53 +0900

    feat : 구매자앱 Auth 페이지 UI 완성 (7-54-58_Auth_UI)

--- CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/user/AuthFragment.kt
+++ CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/user/AuthFragment.kt
@@ -7,17 +7,34 @@ import android.view.View
 import android.view.ViewGroup
 import com.test.campingusproject_customer.R
 import com.test.campingusproject_customer.databinding.FragmentAuthBinding
+import com.test.campingusproject_customer.ui.main.MainActivity
 
 class AuthFragment : Fragment() {
 
-
+    lateinit var fragmentAuthBinding: FragmentAuthBinding
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+
+        fragmentAuthBinding = FragmentAuthBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        fragmentAuthBinding.run {
+            materialToolbarAuth.run {
+                title = "본인 인증"
+
+                setNavigationIcon(androidx.appcompat.R.drawable.abc_ic_ab_back_material)
+                setNavigationOnClickListener {
+                    mainActivity.removeFragment(MainActivity.AUTH_FRAGMENT)
+                }
+            }
+        }
+
+        return fragmentAuthBinding.root
+
     }
 
 }
