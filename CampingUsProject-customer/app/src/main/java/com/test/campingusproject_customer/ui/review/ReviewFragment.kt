f221023f1cd67a2172f894e01678cb7350d2f69e
Do-Re-Min-Woo <kimy5878@naver.com>
2023-08-25 11:31:05 +0900

    feat : 리뷰화면 구성

--- CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/review/ReviewFragment.kt
+++ CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/review/ReviewFragment.kt
@@ -1,13 +1,17 @@
 package com.test.campingusproject_customer.ui.review
 
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.AttributeSet
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import com.test.campingusproject_customer.R
 import com.test.campingusproject_customer.databinding.FragmentReviewBinding
-import com.test.campingusproject_customer.databinding.FragmentReviewDetailBinding
 import com.test.campingusproject_customer.ui.main.MainActivity
 
 class ReviewFragment : Fragment() {
@@ -22,6 +26,29 @@ class ReviewFragment : Fragment() {
         mainActivity = activity as MainActivity
 
         fragmentReviewBinding.run {
+            toolbarReview.run {
+                title = "상품 리뷰"
+
+                // 백버튼
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    mainActivity.removeFragment(MainActivity.REVIEW_FRAGMENT)
+                }
+            }
+
+            linearLayoutReview.run {
+                setOnClickListener {
+                    mainActivity.replaceFragment(MainActivity.REVIEW_DETAIL_FRAGMENT, true, true, null)
+                }
+            }
+
+            linearLayoutReviewSeekBarGroup.run {
+                seekBar1Review.run {
+                }
+                seekBar2Review.run {
+                }
+            }
+
 
         }
 
