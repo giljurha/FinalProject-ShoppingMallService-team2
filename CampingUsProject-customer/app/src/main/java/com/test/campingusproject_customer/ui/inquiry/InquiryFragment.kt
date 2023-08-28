2e9a7e3c41d3f00b975489e6b2a9153a1aa02d78
Do-Re-Min-Woo <kimy5878@naver.com>
2023-08-28 19:47:30 +0900

    feat : 문의 기능 구현

--- CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/inquiry/InquiryFragment.kt
+++ CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/ui/inquiry/InquiryFragment.kt
@@ -1,27 +1,47 @@
 package com.test.campingusproject_customer.ui.inquiry
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import com.google.android.material.snackbar.Snackbar
 import com.test.campingusproject_customer.R
 import com.test.campingusproject_customer.databinding.FragmentInquiryBinding
+import com.test.campingusproject_customer.dataclassmodel.InquiryModel
 import com.test.campingusproject_customer.repository.InquiryRepository
 import com.test.campingusproject_customer.ui.main.MainActivity
-import com.test.campingusproject_customer.viewmodel.InquiryViewModel
-import com.test.campingusproject_customer.viewmodel.ProductViewModel
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class InquiryFragment : Fragment() {
     lateinit var fragmentInquiryBinding: FragmentInquiryBinding
     lateinit var mainActivity: MainActivity
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         fragmentInquiryBinding = FragmentInquiryBinding.inflate(layoutInflater)
         mainActivity = activity as MainActivity
+
+        // SharedPreferences 선언
+        val sharedPreferences = mainActivity.getSharedPreferences("customer_user_info", Context.MODE_PRIVATE)
+        val inquiryUserId = sharedPreferences.getString("customerUserId", null)
+        val inquiryUserName = sharedPreferences.getString("customerUserName", null)
+
+        Log.d("ㅁㅇ", inquiryUserId.toString())
+        Log.d("ㅁㅇ", inquiryUserName.toString())
+
+        // 번들 객체 정보 가져오기
+        val inquiryItemId = arguments?.getLong("productId")
+        val inquiryProductName = arguments?.getString("productName")
+
 
         fragmentInquiryBinding.run {
             toolbarInquiry.run {
@@ -36,7 +56,21 @@ class InquiryFragment : Fragment() {
 
             buttonInquirySubmit.run {
                 setOnClickListener {
-                    InquiryRepository.setInquiryData()
+                    // 현재시간
+                    val current = LocalDateTime.now()
+                    val formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분")
+                    val formatted = current.format(formatter)
+
+                    val inquiryData = InquiryModel(
+                        inquiryItemId!!, inquiryUserId!!, inquiryProductName!!,
+                        editTextTextInquiryContent.text.toString(), inquiryUserName!!, formatted, "답변이 작성되지 않았습니다.", false)
+
+                    Log.d("민우", inquiryData.toString())
+
+                    InquiryRepository.setInquiryData(inquiryData) {
+                        Snackbar.make(fragmentInquiryBinding.root, "문의가 등록되었습니다.", Snackbar.LENGTH_SHORT).show()
+                        mainActivity.removeFragment(MainActivity.INQUIRY_FRAGMENT)
+                    }
                 }
             }
         }
