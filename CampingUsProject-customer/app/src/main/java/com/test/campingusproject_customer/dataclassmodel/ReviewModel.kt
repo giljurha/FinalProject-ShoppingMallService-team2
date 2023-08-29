26567ce39f12a8baa58e7f4a811e1ccbe99380bc
Do-Re-Min-Woo <kimy5878@naver.com>
2023-08-29 17:58:11 +0900

    feat : 리뷰 작성기능 구현

--- CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/dataclassmodel/ReviewModel.kt
+++ CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/dataclassmodel/ReviewModel.kt
@@ -1,12 +1,10 @@
 package com.test.campingusproject_customer.dataclassmodel
 
-import android.net.Uri
-
 data class ReviewModel(
-    var reviewId: String,               // 리뷰 ID
-    var reviewProductId: String,        // 상품 ID
+    var reviewId: Long,                 // 리뷰 ID
+    var reviewProductId: Long,          // 상품 ID
     var reviewWriterId: String,         // 리뷰 작성자 ID
-    var reviewRating: String,           // 리뷰 별점
-    var reviewImage: Uri,               // 리뷰 사진
-    var reviewRecommendation: String    // 상품 추천
+    var reviewRating: Float,            // 리뷰 별점
+    var reviewImage: String,            // 리뷰 사진
+    var reviewContent: String           // 리뷰 내용
 )
--- CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/repository/ReviewRepository.kt
+++ CampingUsProject-customer/app/src/main/java/com/test/campingusproject_customer/repository/ReviewRepository.kt
@@ -1,5 +1,60 @@
 package com.test.campingusproject_customer.repository
 
-class ReviewRepository {
+import android.net.Uri
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+import com.test.campingusproject_customer.dataclassmodel.ReviewModel
 
+class ReviewRepository {
+    companion object {
+        // 리뷰 인덱스를 DB에 저장
+        fun setReviewIdx(reviewId: Long, callback1: (Task<Void>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val reviewIdRef = database.getReference("ReviewIdx")
+
+            reviewIdRef.get().addOnCompleteListener {
+                it.result.ref.setValue(reviewId).addOnCompleteListener(callback1)
+            }
+        }
+
+        // 리뷰 인덱스를 DB에서 가져온다.
+        fun getReviewIdx(callback1: (Task<DataSnapshot>) -> Unit) {
+            val database = FirebaseDatabase.getInstance()
+            val reviewIdRef = database.getReference("ReviewIdx")
+
+            reviewIdRef.get().addOnCompleteListener(callback1)
+        }
+
+        // 리뷰를 DB에 추가하는 함수
+        fun setReviewInfo(reviewModel : ReviewModel, callback1 : (Task<Void>) -> Unit){
+            val database = FirebaseDatabase.getInstance()
+
+            val productRef = database.getReference("ReviewData")
+            productRef.push().setValue(reviewModel).addOnCompleteListener(callback1)
+        }
+
+        //상품 이미지들을 업로드하는 함수
+        fun uploadImages(uploadUri : MutableList<Uri>, fileDir : String, callback1: (Task<UploadTask.TaskSnapshot>) -> Unit){
+            val storage = FirebaseStorage.getInstance()
+
+            val count = uploadUri.size
+            for(idx in 0 until count){
+                val fileName = fileDir + "${idx+1}.png"
+                val imageRef = storage.reference.child(fileName)
+                imageRef.putFile(uploadUri[idx]).addOnCompleteListener(callback1)
+            }
+        }
+
+        //상품의 대표이미지만 가져오는 함수
+        fun getProductFirstImage(fileDir:String, callback1: (Task<Uri>) -> Unit){
+            val storage = FirebaseStorage.getInstance()
+            val fileName = fileDir + "1.png"
+
+            val imageRef = storage.reference.child(fileName)
+            imageRef.downloadUrl.addOnCompleteListener(callback1)
+        }
+    }
 }
