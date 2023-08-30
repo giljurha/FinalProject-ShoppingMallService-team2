1932454157b4c096a074a1c101dbb0c27f3856a3
y-d-h <ehdgh8777@gmail.com>
2023-08-30 10:39:17 +0900

    fix : 구매자용 inquiry fragment 수정

--- CampingUsProjectseller/app/src/main/java/com/test/campingusproject_seller/dataclassmodel/InquiryModel.kt
+++ CampingUsProjectseller/app/src/main/java/com/test/campingusproject_seller/dataclassmodel/InquiryModel.kt
@@ -4,7 +4,7 @@ data class InquiryModel(
     var inquiryIdx: Long,             // 문의 IDX
     var inquiryItemId: Long,          // 제품 아이템 ID
     var inquiryUserId: String,        // 문의 작성자 ID
-    var inquiryItemTitle: String,     // 문의 아이템(제목)
+    var inquiryProductName: String,     // 문의 아이템(제목)
     var inquiryContent: String,       // 문의 내용
     var inquiryUserName: String,      // 문의 작성자 이름
     var inquiryWriteDate: String?,    // 문의 작성 날짜
